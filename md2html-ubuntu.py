36cf824f1f21e7a6d5ebffb90cd508d0b683902c
dennishnf <dennishnf@gmail.com>
2020-11-21 06:56:50 -0500

    making website

--- md2html-ubuntu.py
+++ md2html-ubuntu.py
@@ -199,10 +199,10 @@ def convert(pathh):
     
     with open(path_out, "a") as myfile:
           timee=strftime("%d-%m-%Y %H:%M", gmtime())
-          myfile.write("<a href=\"https://dennishnf.com/README.html\" target=\"_blank\">Made from scratch - Python as backend</a> <br/>\n")
           myfile.write("Website updated ")
           myfile.write(timee)
           myfile.write(" GMT <br/> \n</div> \n")
+          myfile.write("<a href=\"https://dennishnf.com/README.html\" target=\"_blank\">Made from scratch - Python as backend</a> <br/>\n")
           myfile.write("<div class=\"clearer\">&nbsp;</div> \n")
           myfile.write("</div> \n")
           myfile.write("</div> \n")
