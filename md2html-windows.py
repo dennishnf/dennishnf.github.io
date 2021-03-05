b9699da3722256ace997016c2c51f1fb3d39adc5
Dennis Núñez Fernández <dennishnf@gmail.com>
2021-03-05 18:12:42 -0500

    making website

--- md2html-windows.py
+++ md2html-windows.py
@@ -198,7 +198,7 @@ def convert(pathh):
               output.write(data)
     
     with open(path_out, "a") as myfile:
-          timee=strftime("%d-%m-%Y %H:%M", gmtime())
+          timee=strftime("%Y-%m-%d %H:%M", gmtime())
           myfile.write("<a href=\"https://dennishnf.com/README.html\" target=\"_blank\">Made from scratch - Python as backend</a> <br/>\n")
           myfile.write("Website updated ")
           myfile.write(timee)
